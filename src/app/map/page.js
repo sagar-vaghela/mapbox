22328a11a8984b88b332df5cd3f3bc3091bea59d
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2023-12-28 15:07:30 +0530

    added mapbox

---
+++ src/app/map/page.js
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FnYXIxOXZhZ2hlbGEiLCJhIjoiY2xxcDAxb2R3MHVhMTJrc2JrcjNlOHUweSJ9.t9XmVbDJLtV--Lpb9pcDOA';
+
+export default function Map() {
+
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+    });
+
+    return (
+        <div>
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
+}
